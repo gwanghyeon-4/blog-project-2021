f1a69adc7c8fa3a0de034093d71063276e9fb41b
gwanghyeon <nogwanghyeon34@gmail.com>
2021-02-25 23:26:13 +0900

    first

---
+++ src/controller/boardController.js
@@ -0,0 +1,132 @@
+import Post from "../models/Post";
+import PostType from "../models/PostType";
+import {globalController} from "../controller/globalController";
+import mongoose from "mongoose";
+import middlewares from "../common/middlewares";
+
+const detailController = async (req, res) => {
+  try {
+    const postData = await Post.findOne({ _id: req.params.id });
+
+    const currentHit = postData.hit;
+    const nextHit = parseInt(currentHit) + 1;
+
+    await Post.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          hit: nextHit,
+        },
+      }
+    );
+
+    const devMode = middlewares.checkDevMode();
+
+    res.render("screens/boardDetail", { postData, devMode });
+  } catch (e) {
+    console.log(e);
+    res.render("screens/home");
+  }
+};
+
+const boardWriteController = (req, res) => {
+  const type = req.params.type;
+
+  res.render("screens/boardWrite", { type });
+};
+
+const boardWriteDbController = async (req, res) => {
+  let searchType = "";
+
+  if (req.body.type === "javascript") {
+    searchType = "JS";
+  }
+
+  try {
+    const type = await PostType.findOne({ typeName: searchType });
+
+    const currentTime = new Date().toString();
+    const allPost = await Post.find();
+    const postNo = allPost.length + 1;
+
+    const newPostTypeId = mongoose.Types.ObjectId(type._id);
+
+    const result = await Post.create({
+      title: req.body.title,
+      description: req.body.desc,
+      author: `관리자`,
+      hit: 0,
+      postType: newPostTypeId,
+      createdAt: currentTime,
+      lastUpdatedAt: currentTime,
+      isDelete: false,
+      no: postNo,
+    });
+
+    console.log("❌❌❌❌❌❌❌❌❌❌❌❌");
+    console.log(result);
+    console.log("❌❌❌❌❌❌❌❌❌❌❌❌");
+  } catch (e) {
+    console.log(e);
+  }
+
+  globalController.javascriptController(req, res);
+};
+
+const deleteBoardController = async (req, res) => {
+  console.log(req.body.id);
+  try {
+    const result = await Post.updateOne(
+      { _id: req.body.id },
+      {
+        $set: {
+          isDelete: true,
+        },
+      }
+    );
+  } catch (e) {
+    console.log(e);
+  }
+
+  globalController.javascriptController(req, res);
+};
+
+const boardUpdateController = async (req, res) => {
+  try {
+    const board = await Post.findOne({ _id: req.body.id });
+
+    res.render("screens/boardUpdate", { board });
+  } catch (e) {
+    console.log(e);
+    globalController.javascriptController(req, res);
+  }
+};
+
+const updateController = async (req, res) => {
+  const {
+    body: { id, title, desc },
+  } = req;
+  try {
+    const result = await Post.updateOne(
+      { _id: id },
+      {
+        $set: {
+          title: title,
+          description: desc,
+        },
+      }
+    );
+  } catch (e) {
+    console.log(e);
+  }
+  globalController.javascriptController(req, res);
+};
+
+export const boardController = {
+  detailController,
+  boardWriteController,
+  boardWriteDbController,
+  deleteBoardController,
+  boardUpdateController,
+  updateController,
+};
---
+++ src/controller/globalController.js
@@ -0,0 +1,40 @@
+import Post from "../models/Post";
+import PostType from "../models/PostType";
+import dotenv from "dotenv";
+dotenv.config();
+import middlewares from "../common/middlewares";
+
+const homeController = (req, res) => {
+  console.log("I'm home Controller");
+  res.render("screens/home");
+};
+
+const contactController = (req, res) => {
+  res.render("screens/contact");
+};
+const javascriptController = async (req, res) => {
+  try {
+    const dataResult = await Post.find({ isDelete: false }).populate({
+      path: "PostType",
+      model: PostType,
+    });
+
+    const devMode = middlewares.checkDevMode();
+
+    res.render("screens/javascript", { dataResult, devMode });
+  } catch (e) {
+    console.log(e);
+    res.render("screens/home");
+  }
+};
+
+const boardwriteController = (req, res) => {
+  res.render("screens/boardWrite");
+};
+
+export const globalController = {
+  homeController,
+  contactController,
+  javascriptController,
+  boardwriteController,
+};
